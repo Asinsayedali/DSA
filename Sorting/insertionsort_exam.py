c7f3f731f7b6ff50134e7939c0de71bf1b9593b8
Asinsayedali <asinsayedali@gmail.com>
2025-12-13 12:02:43 +0530

    Added insertion sort example

---
+++ Sorting/insertionsort_exam.py
@@ -0,0 +1,16 @@
+n = int(input("enter the size of the array\n"))
+array = []
+for i in range(n):
+    value = int(input("enter the element \n"))
+    array.append(value)
+
+# insertion sort implementation
+for i in range(len(array)):
+    j = i-1
+    while j>=0 and array[j+1] < array[j]:
+        temp = array[j+1]
+        array[j+1] = array[j]
+        array[j]=temp
+        j = j -1 
+
+print(array)
