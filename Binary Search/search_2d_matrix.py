8c4f25c6680423cd1c1bd52b7fe1e86e8176e795
Asinsayedali <asinsayedali@gmail.com>
2026-01-16 00:13:27 +0530

    Added new problem

--- Binary Search/search_2d_matrix.py
+++ Binary Search/search_2d_matrix.py
@@ -22,4 +22,26 @@ class Solution:
         
 
 # OPTIMIZED SOLUTION WHERE COMPLEXITY IS IN O(LOG(M*N))
+# Core idea is we treat the 2d array as a 1d array and map the mid to find the row and column in the 2d array. 
+# That is row value is always mid//col in 2d array.. the coloumn no of the value will be always mid%col.
+# inshort mid//col gets which row the element is in, mid%col gets which column the element is in. So we get the i and j value to see if value matches the target. 
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        row = len(matrix)
+        col = len(matrix[0])
 
+        left = 0 
+        right = (row * col) -1
+
+        while left<=right:
+            mid = (left+right)//2
+            r = mid // row
+            c = mid % col
+            value = matrix[r][c]
+            if value < target:
+                left = mid + 1
+            elif value > target:
+                right = mid - 1
+            else:
+                return True
+        return False
