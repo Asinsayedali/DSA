5fb8883de597cd76afbdac8a53f6bac94c68c0e1
Asinsayedali <asinsayedali@gmail.com>
2026-05-11 12:22:22 +0530

    Updated the BST

---
+++ Tree/insertbst.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return TreeNode(val)
+        if val > root.val:
+            root.right = self.insertIntoBST(root.right,val)
+        elif val < root.val:
+            root.left = self.insertIntoBST(root.left,val)
+        return root
+        
+#General solution
+def insert(root, val):
+    if not root:
+        return TreeNode(val)
+    if val > root.val:
+        root.right = insert(root.right,val)
+    elif val < root.val:
+        root.left = insert(root.left,val)
+    return root
