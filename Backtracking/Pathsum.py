37d398c264c68da49e5c399441843e96aead5ec1
Asinsayedali <asinsayedali@gmail.com>
2026-05-14 14:26:10 +0530

    Updated backtracking

--- Backtracking/Pathsum.py
+++ Backtracking/Pathsum.py
@@ -20,3 +20,25 @@ class Solution:
             path.pop()
             return False
         return roottoleaf(root,path)
+    
+# another solutions which doesn't use a list to store the path but instead uses a variable to keep track of the sum of the path
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+
+        def dfs(node, remaining):
+            if not node:
+                return False
+            remaining -= node.val
+            if not node.left and not node.right:
+                return remaining == 0
+
+            return dfs(node.left, remaining) or dfs(node.right, remaining)
+
+        return dfs(root, targetSum)
